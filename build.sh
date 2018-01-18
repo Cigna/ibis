9b5f2a5c9d30d162af832888fa309a4cf22bf0bb
m55551 <shivakumar.athreya@accenture.com>
2018-01-18 13:15:22 -0500

    Update build process

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+ibis_home=$1
+args=$2
+cur_dir=`pwd`
+echo 'ibis_home ' $ibis_home
+echo 'args' $args
+export ibis_home=$ibis_home
+export cur_dir=$cur_dir
 
-setup_env(){
-	# install Requirements
-	pip install -r ./ibis/requirements.pip
+setup_env() {
+	# install Requirements 
+	# pip install -r $ibis_home/requirements.pip
 	
 	echo "*************"
 	pip show impyla
-	echo 
+	echo
 	pip show thrift
 	echo "*************"
 	
@@ -24,71 +31,102 @@ setup_env(){
 }
 
 run_unit_tests() {
-
+	
 	echo -e "<i><b>Test bold</b></i>"
 	# Run test cases and capture Coverage Report
-	nosetests ./ibis/ibis_test_suite.py --with-xunit
-	
-	coverage run --branch --source="ibis" --omit="*test*,ibis/features/*,ibis/setup.py,ibis/__main__.py,*__init__.py*,ibis/import_version.py,ibis/ibis/settings.py,ibis/ibis/utilities/run_parallel.py,ibis/ibis/utilities/gitlab.py,ibis/ibis/ingest/import_prep.py" ./ibis/ibis_test_suite.py
+	nosetests $ibis_home/ibis_test_suite.py --with-xunit
+    let EXIT_STATUS_PY=$?
+	if [ "$EXIT_STATUS_PY" -gt 0 ]
+	then
+		echo ' test cases failed'
+		exit 1
+	fi
+	coverage run --branch --source="ibis" --omit="*test*,ibis/features/*,ibis/setup.py,ibis/__main__.py,*__init__.py*,ibis/import_version.py,ibis/ibis/settings.py,ibis/ibis/utilities/run_parallel.py,ibis/ibis/utilities/gitlab.py,ibis/ibis/ingest/import_prep.py" $ibis_home/ibis_test_suite.py
 	coverage report
 	coverage xml
 	coverage html -d coverage-report
+	mv $ibis_home/test_logs $cur_dir/
+	mv $ibis_home/test_workflows-git $cur_dir/
+	mv $ibis_home/test_files $cur_dir/
 }
 
-check_code_quality(){
+check_code_quality() {
 
 	# Run codestyle and pylint to capture Code style violations
 	pip install pycodestyle
-	$( pycodestyle ibis | tee  pep8.out ) ||  echo 'find issues' | exit -1
+	$( pycodestyle $ibis_home/ibis | tee  pep8.out ) ||  echo 'find issues' | exit -1
 	pepper8 -o pep8_report.html pep8.out
 	
-	cd ibis_home
-	pylint --rcfile=.pylintrc --output-format=parseable ibis lib features | tee pylint.out
+	pylint --rcfile=.pylintrc --output-format=parseable $ibis_home/ibis $ibis_home/lib $ibis_home/features | tee pylint.out
 }
 
 check_security(){
 	# Run Bandit for security check
-	cd ibis_home
+	cd $ibis_home
 	cd ..
-	bandit -f html -o bandit_results.html -r ibis -lll
-	cd ibis_home
+	ibis_pre_dir=`pwd`
+	bandit -f html -o bandit_results.html -r $ibis_pre_dir/ibis -lll
+	cd $cur_dir
 }
 
 build_egg(){
+
 	#Build the egg
-	cd ibis_home
-	python2.7 setup.py bdist_egg
+	rm -rf $cur_dir/dist
 	
-	cp requirements.pip ./dist
-	cp ibis-shell ./dist/
-	cp ibis_version.sh ./dist/
-	cp -R ./lib ./dist/lib
-	
-	tar -czvf ibis.tar.gz -C ./dist .
-	
+	cd $ibis_home
+	python2.7 $ibis_home/setup.py bdist_egg
+    let EXIT_STATUS_PY=$?
+	if [ "$EXIT_STATUS_PY" -gt 0 ]
+	then
+		echo ' build failed'
+		exit 1
+	fi
+	mv $ibis_home/dist $cur_dir
+	mv $ibis_home/opensource_ibis.egg-info $cur_dir
+	mv $ibis_home/build $cur_dir
+	cp requirements.pip $cur_dir/dist
+	cp ibis-shell $cur_dir/dist
+	cp ibis_version.sh $cur_dir/dist
+	cp -R ./lib $cur_dir/dist/lib
+
+	tar -czvf $cur_dir/ibis.tar.gz -C $cur_dir/dist .
+	cd $cur_dir
+
 }
 
 main() {
 
-export ibis_home=$1
-setup_env
-
-if [-z ${$2+x} ]; then
-run_unit_tests
-check_code_quality
-check_security
-build_egg
-
-if [$2 == 'skip-all-test']; then
-build_egg
-
-if [$2 == 'skip-code-check']; then
-run_unit_tests
-build_egg
-
-if [$2 == 'skip-build']; then
-run_unit_tests
-check_code_quality
-check_security
+	echo 'inside' $ibis_home
+	setup_env
+	
+	if [ "${args}" == "" ]; then
+		run_unit_tests
+		check_code_quality
+		check_security
+		build_egg
+	elif [ "${args}" == "skip-all-test" ]; then
+		build_egg
+	elif [ "${args}" == "skip-code-check" ]; then
+		run_unit_tests
+		build_egg
+	elif [ "${args}" == "skip-build" ]; then
+		run_unit_tests
+		check_code_quality
+		check_security
+	else
+		echo '========================ATTENTION===================================='
+		echo 'Argument 1 : needs to be IBIS home directory path'
+		echo 'Argument 2 : Needs to be blank or if provided has to the below'
+		echo '             skip-all-test - Skip all validation and create Egg'
+		echo '             skip-code-check - Skip code style check'
+		echo '             skip-build - Run all validations and skip build process'
+		echo '====================================================================='
+	fi
 
 }
+
+main
+
+# WRITE CODE ABOVE THIS LINE
+# WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING
