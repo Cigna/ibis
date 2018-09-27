da03c66117b6f15d49e31dd7a18fab4f1e725f15
M55551 <shivakumar.athreya@cigna.com>
2018-09-27 10:04:04 -0400

    fix testcases

--- ibis/utilities/tests/test_config_manager.py
+++ ibis/utilities/tests/test_config_manager.py
@@ -22,42 +22,6 @@ class ConfigManagerTest(unittest.TestCase):
         self.assertTrue(isinstance(_, ConfigManager))
 
     @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_dev_perf_props(self, m_set):
-        """test dev_perf properties file"""
-        _ = ConfigManager('dev_perf', 'dev_perf', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_int_props(self, m_set):
-        """test int properties file"""
-        _ = ConfigManager('int', 'dev', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_int_test_props(self, m_set):
-        """test int_test properties file"""
-        _ = ConfigManager('int_test', 'dev', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_prod_props(self, m_set):
-        """test prod properties file"""
-        _ = ConfigManager('prod', 'dev', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_perf_props(self, m_set):
-        """test perf properties file"""
-        _ = ConfigManager('perf', 'dev', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
-    def test_jenkins_props(self, m_set):
-        """test jenkins properties file"""
-        _ = ConfigManager('jenkins', 'dev', True)
-        self.assertTrue(isinstance(_, ConfigManager))
-
-    @patch.object(ConfigManager, 'create_path', autospec=True)
     def test_unit_test_props(self, m_set):
         """test unit_test properties file"""
         _ = ConfigManager('unit_test', 'dev', True)
